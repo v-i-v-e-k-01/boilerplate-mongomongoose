e3684b97d2b3c8ef350085ddfba58e85d1acce20
Vivek D <vivek01110110@gmail.com>
2024-04-03 17:46:24 +0000

    modified:   myApp.js

--- myApp.js
+++ myApp.js
@@ -1,9 +1,11 @@
-require('dotenv').config();
-
+let dotenv=require('dotenv');
+let mongoose = require('mongoose');
+dotenv.config();
+mongoose.connect(process.env.MONGO_URI,{useNewUrlParser: true, useUnifiedTopology: true});
 
 let Person;
 
-const createAndSavePerson = (done) => {
+const createAndSavePerson = (done) => { 
   done(null /*, data*/);
 };
 
