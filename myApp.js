bd90a51e7f20af1c99bb4a43e9cad2c8c2e26ff0
Vivek D <91778440+v-i-v-e-k-01@users.noreply.github.com>
2024-04-05 21:15:59 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,11 +1,16 @@
-let dotenv=require('dotenv');
+require('dotenv').config();
 let mongoose = require('mongoose');
-dotenv.config();
-mongoose.connect(process.env.MONGO_URI,{useNewUrlParser: true, useUnifiedTopology: true});
 
-let Person;
+mongoose.connect(process.env.MONGO_URI,{useNewUrlParser:true , useUnifiedTopology:true});
 
-const createAndSavePerson = (done) => { 
+const personSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String]
+});
+const Person = mongoose.model("Person", personSchema);
+
+const createAndSavePerson = (done) => {
   done(null /*, data*/);
 };
 
