040299118783fec0483e025270194f4cb7b668c6
Vivek D <91778440+v-i-v-e-k-01@users.noreply.github.com>
2024-04-06 23:15:53 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -71,10 +71,14 @@ const findEditThenSave = (personId, done) => {
   });
 };
 
+
+
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-
-  done(null /*, data*/);
+  Person.findOneAndUpdate({name:personName},{age:ageToSet},{new:true, runValidators:true},function(err,person){
+    if(err) return console.error(err);
+    done(null, person);
+    });
 };
 
 const removeById = (personId, done) => {
